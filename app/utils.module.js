3e2a224e82bebe392631ef0ad365397cb65a1f34
Anton I <anton.ilchenko@zyfra.com>
2021-04-04 23:19:59 +0200

    FIX: Fixed cards randomization

--- app/utils.module.js
+++ app/utils.module.js
@@ -15,21 +15,20 @@ export default class Utils {
     const maxEl = this.stackLength - 1;
     let index = 0;
 
+    // Create array of Randomized indexes
+    const indexes = Array.from(Array(this.stackLength).keys())
+      .sort(() => Math.random() - .5);
+
+    let cur = 0;
+
     for (let i = 0; i < this.types; i++) {
       for (let j = 0; j < this.colors; j++) {
         for (let k = 0; k < this.nums; k++) {
           for (let l = 0; l < this.fills; l++) {
             // Get random index
-            index = Math.floor(Math.random() * maxEl);
-
-            // Check if this index is filled and find next free index
-            while (!!cache[index]) {
-              index++;
-              if (index > maxEl) index = 0;
-            }
-
-            // Insert current card into the index
+            index = indexes[cur];
             cache[index] = [i, j, k, l];
+            cur++;
           }
         }
       }
